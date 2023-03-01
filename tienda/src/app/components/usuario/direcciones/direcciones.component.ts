e2c2474b96d46b600665099314119ba666225268
Externo Lucas Brieva <lucas.brieva.ext@uss.cl>
2023-03-01 01:20:06 -0300

    Cuenta principal y formulario dirección

--- tienda/src/app/components/usuario/direcciones/direcciones.component.ts
+++ tienda/src/app/components/usuario/direcciones/direcciones.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ClienteService } from 'src/app/services/cliente.service';
 import { GuestService } from 'src/app/services/guest.service';
+import { HelperService } from 'src/app/services/helper.service';
 
 declare var jquery: any;
 declare var $: any;
@@ -21,12 +23,31 @@ export class DireccionesComponent implements OnInit {
   public regiones: Array<any> = [];
   public provincias: Array<any> = [];
   public ciudades: Array<any> = [];
+  public regiones_arr: Array<any> = [];
+  public provincias_arr: Array<any> = [];
+  public ciudades_arr: Array<any> = [];
 
   constructor(
-    private _guestService: GuestService
+    private _guestService: GuestService,
+    private _helperService: HelperService,
+    private _clienteService : ClienteService
   ) {
     this.token = localStorage.getItem('token');
-    
+    this._guestService.get_regiones().subscribe(
+      res => {
+        this.regiones_arr = res;
+      }
+    );
+    this._guestService.get_provincias().subscribe(
+      res => {
+        this.provincias_arr = res;
+      }
+    );
+    this._guestService.get_ciudades().subscribe(
+      res => {
+        this.ciudades_arr = res;
+      }
+    )
   }
 
   ngOnInit(): void {
@@ -91,5 +112,55 @@ export class DireccionesComponent implements OnInit {
         });
       }
     )
+  }
+  registrar(registroForm){
+    if(registroForm.valid){
+      //TODO: Esto lo puedo reemplazar por un find. 
+      this.regiones_arr.forEach(e=>{
+        if(parseInt(e.id) == parseInt(this.direccion.region)){
+          this.direccion.region = e.name;
+        }
+      });
+      this.provincias_arr.forEach(e=>{
+        if(parseInt(e.id) == parseInt(this.direccion.provincia)){
+          this.direccion.provincia = e.name;
+        }
+      });this.ciudades_arr.forEach(e=>{
+        if(parseInt(e.id) == parseInt(this.direccion.ciudad)){
+          this.direccion.ciudad = e.name;
+        }
+      });
+      let data = {
+        destinatario: this.direccion.destinatario,
+        dni: this.direccion.dni,
+        zip: this.direccion.zip,
+        direccion: this.direccion.direccion,
+        telefono: this.direccion.telefono,
+        pais: this.direccion.pais,
+        region: this.direccion.region,
+        provincia: this.direccion.provincia,
+        ciudad: this.direccion.ciudad,
+        principal: this.direccion.principal,
+        cliente: localStorage.getItem('_id')
+      }
+
+      this._clienteService.registro_direccion_cliente(data, this.token).subscribe(
+        res=>{
+          this.direccion = {
+            pais: '',
+            region: '',
+            provincia: '',
+            ciudad: '',
+            principal: false
+          };
+          this._helperService.iziToast("La dirección ha sido agregada correctamente", "Actualizado", true);
+        },
+        err=>{
+
+        }
+      );
+    }else{
+      this._helperService.iziToast('Los datos del formulario no son válidos', 'ERROR', false);
+    }
   }
 }
